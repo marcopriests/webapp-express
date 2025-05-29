8e86f215e7aef61699b7e39009bff177715e5804
marcopriests <prtmrc98@gmail.com>
2025-05-29 16:47:59 +0200

    [UPD] show query fatta

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -3,14 +3,21 @@ const connection = require('../data/db')
 const index = (req, res) => {
     const sql = 'SELECT * FROM movies'
     connection.query(sql, (err, movieResult) => {
-        if (err) res.status(500).json({ error: `Database query failed: ${err.message}` })
+        if (err) return res.status(500).json({ error: `Database query failed` })
 
         res.json(movieResult)
     })
 }
 
 const show = (req, res) => {
-    res.send('Dettaglio film')
+    const sql = 'SELECT * FROM movies WHERE id = ?'
+    const { id } = req.params
+    connection.query(sql, [id], (err, movieResult) => {
+        if (err) return res.status(500).json({ error: `Database query failed` })
+        if (movieResult.length === 0) return res.status(404).json({ error: `Movie not found` })
+
+        res.json(movieResult)
+    })
 }
 
 module.exports = {
