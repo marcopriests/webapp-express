ce472b03bb75084528e3b1ea489721333a5fc7c7
marcopriests <prtmrc98@gmail.com>
2025-06-04 13:00:08 +0200

    [UPD]

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -19,7 +19,7 @@ const show = (req, res) => {
     const movieSql = 'SELECT * FROM movies WHERE id = ?'
     const reviewSql = 'SELECT * FROM reviews WHERE movie_id = ?'
 
-    const avgVote = 'SELECT M.*, ROUND(AVG(R.vote)) AS average_vote FROM movies M JOIN reviews R ON R.movie_id = M.id WHERE M.id = ?'
+    const avgVote = 'SELECT M.*, ROUND(AVG(R.vote)) AS average_vote FROM movies M LEFT JOIN reviews R ON R.movie_id = M.id WHERE M.id = ?'
 
     const { id } = req.params
 
@@ -41,6 +41,23 @@ const show = (req, res) => {
     })
 }
 
+const store = (req, res, next) => {
+    const { title, director, abstract } = req.body
+
+    const sql = 'INSERT INTO movies (title, director, abstract, image) VALUES (?,?,?,?)'
+
+    const imageName = req.file.filename
+
+    connection.query(sql, [title, director, abstract, imageName], (err, result) => {
+        if (err) { return next('Errore di caricamento') }
+
+        res.status(201).json({
+            status: 'success',
+            message: 'film inserito con successo'
+        })
+    })
+}
+
 module.exports = {
-    index, show
+    index, show, store
 }
--- routers/movieRouter.js
+++ routers/movieRouter.js
@@ -2,10 +2,14 @@ const express = require('express')
 
 const router = express.Router()
 
+const upload = require('../middlewares/multer')
+
 const movieController = require('../controllers/movieController')
 
 router.get('/', movieController.index)
 
 router.get('/:id', movieController.show)
+
+router.post('/', upload.single('image'), movieController.store)
 
 module.exports = router
